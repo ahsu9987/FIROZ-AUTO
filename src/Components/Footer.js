079b83e7c670aae45d25ff12ff99718c503554bc
ahsu9987 <ahsanansari75862@gmail.com>
2025-08-03 22:29:23 +0530

    update home page is ready

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,67 +7,70 @@ import whatsaplogo from "../Pages/HomeAllimages/Alllogo/whatsapplogo.png";
 import instalogo from "../Pages/HomeAllimages/Alllogo/instalogo.png";
 import fblogo from "../Pages/HomeAllimages/Alllogo/facebooklogo.png";
 import calllogo from "../Pages/HomeAllimages/Alllogo/calllogo.png";
+import call from "../Pages/HomeAllimages/Alllogo/call.png";
+import mail from "../Pages/HomeAllimages/Alllogo/mail.png";
 import { motion } from "framer-motion";
-
 
 function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-300 pt-12">
       <Container className="mb-5 mt-5">
-         <motion.h1
-            initial={{ opacity: 0, scale: 0.9 }}
+        <motion.h1
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.9,
+            delay: 0.2,
+            ease: [0, 0.4, 0.2, 1.01],
+          }}
+          className=" reachcolor text-xl font-semibold text-white text-center mb-4"
+        >
+          Reach out to us on
+        </motion.h1>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.9,
+            delay: 0.7,
+            ease: [0, 0.4, 0.2, 1.01],
+          }}
+        >
+          <Row
+            className="text-light"
+            initial={{ opacity: 0, scale: 0.5 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 0.9,
-              delay: 0.2,
+              delay: 0.7,
               ease: [0, 0.4, 0.2, 1.01],
             }}
-            className=" reachcolor text-xl font-semibold text-white text-center mb-4"
           >
-            Reach out to us on
-          </motion.h1>
-        <motion.div 
-         initial={{ opacity: 0, scale: 0.5 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 0.9,
-                delay: 0.7,
-                ease: [0, 0.4, 0.2, 1.01],
-              }}
-        >
-<Row className="text-light"
-           initial={{ opacity: 0, scale: 0.5 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 0.9,
-                delay: 0.7,
-                ease: [0, 0.4, 0.2, 1.01],
-              }}
-        >
-         
-          <Col xs={3}
-          >
-            <a href="whatsapp:contact=7678094646@s.whatsapp.com&message=I'd like to chat with you">
-            <Image src={whatsaplogo} alt="Logo" className="footerlogo" />
-            </a>
-          </Col>
-          <Col xs={3}>
-            {" "}
-            <Image src={instalogo} alt="Logo" className="footerlogo" />
-          </Col>
-          <Col xs={3}>
-            {" "}
-            <Image src={fblogo} alt="Logo" className="footerlogo" />
-          </Col>
-          <Col xs={3}>
-            {" "}
-            <a href="tel:767-809-4646">
-            <Image src={calllogo} alt="Logo 767-809-4646" className="footerlogo" />
-            </a>
-          </Col>
-        </Row>
+            <Col xs={3}>
+              <a href="whatsapp:contact=7678094646@s.whatsapp.com&message=I'd like to chat with you">
+                <Image src={whatsaplogo} alt="Logo" className="footerlogo" />
+              </a>
+            </Col>
+            <Col xs={3}>
+              {" "}
+              <Image src={fblogo} alt="Logo" className="footerlogo" />
+            </Col>
+            <Col xs={3}>
+              {" "}
+              <Image src={instalogo} alt="Logo" className="footerlogo" />
+            </Col>
+            <Col xs={3}>
+              {" "}
+              <a href="tel:767-809-4646">
+                <Image
+                  src={calllogo}
+                  alt="Logo 767-809-4646"
+                  className="footerlogo"
+                />
+              </a>
+            </Col>
+          </Row>
         </motion.div>
-        
       </Container>
       <div className="container mx-auto px-4">
         {/* ---------- Top section ---------- */}
@@ -75,7 +78,7 @@ function Footer() {
           {/* Contact & social icons */}
           <div className="text-light">
             <motion.h2
-             initial={{ x: -100, opacity: 0 }}
+              initial={{ x: -100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
               transition={{
                 delay: 0.2,
@@ -84,11 +87,39 @@ function Footer() {
                 ease: "easeIn",
                 duration: 1,
               }}
-            className="reachcolor text-xl font-semibold text-white mb-4">
+              className="reachcolor text-xl font-semibold text-white mb-4 mt-5"
+            >
               Contact
             </motion.h2>
-            <p className="text-sm">Phone:<a href="tel:767-809-4646" className="text-success"  style={{textDecorationLine:"none"}}>+91-76780 94646</a> / <a href="tel:843-352-5372" className="text-success"  style={{textDecorationLine:"none"}}>+91‑8433525372</a></p>
-            <p className="text-sm mb-4"  >Email:-<a href="mailto:firozautorides@gmail.com" style={{textDecorationLine:"none"}}><span   className="text-info" >firozautorides@gmail.com</span></a> </p>
+            <p className="text-sm mt-3">
+                                                        <Image src={call} alt="Logo"  />
+ 
+              <a
+                href="tel:767-809-4646"
+                className="text-success"
+                style={{ textDecorationLine: "none" }}
+              >
+                  +91-76780 94646
+              </a>{" "}
+              /{" "}
+              <a
+                href="tel:843-352-5372"
+                className="text-success"
+                style={{ textDecorationLine: "none" }}
+              >
+                +91‑8433525372
+              </a>
+            </p>
+            <p className="text-sm mb-4 mt-3">
+                                          <Image src={mail} alt="Logo"  />
+
+              <a
+                href="mailto:firozautorides@gmail.com"
+                style={{ textDecorationLine: "none" }}
+              >
+                <span className="text-info"> firozautorides@gmail.com</span>
+              </a>{" "}
+            </p>
           </div>
         </div>
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,69 +2,86 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom/cjs/react-router-dom";
-import logo from "../Pages/HomeAllimages/Alllogo/logofirozauto.jpeg";
+import logo from "../Pages/HomeAllimages/Alllogo/logofirozauto.png";
 import Image from "react-bootstrap/Image";
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import Dropdown from 'react-bootstrap/Dropdown';
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function Header() {
   return (
     <>
-     {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-      <Navbar
-        
-        fixed="top"
-        key={expand} expand={expand} className="bg-body-tertiary mb-3"
-         style={{backgroundColor:"black"}}
-      >
-        <Container>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                    <Image src={logo} className="headerlogo" alt="Logo" />
+      {[false, "sm", "md", "lg", "xl", "xxl"].map((expand) => (
+        <Navbar
+          fixed="top"
+          key={expand}
+          expand={expand}
+          className="bg-body-tertiary mb-3"
+          style={{ backgroundColor: "black" }}
+        >
+          <Container>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Image src={logo} className="headerlogo" alt="Logo" />
 
-<Navbar.Offcanvas
+            <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement="end"
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                   <h4 className="fw-bold" >
-            FIROZ   <span className="text-danger">AUTORIDES</span>
-            </h4>
+                  <h4 className="fw-bold">
+                    FIROZ <span className="text-danger">AUTORIDES</span>
+                  </h4>
                 </Offcanvas.Title>
               </Offcanvas.Header>
-          <Navbar.Collapse id="navbarScroll">
-           
-            <Nav className=" ms-auto my-3 my-lg-0 fw-bold " navbarScroll>
-              <NavLink to="/" className="ms-3 topnavlink">
-                HOME
-              </NavLink>
-              <NavLink to="/Newbikes" className="ms-3 topnavlink">
-                NEW BIKES
-              </NavLink>
-              <NavLink to="" className="ms-3 topnavlink">
-                NEWS&REVIEW
-              </NavLink>
-              <NavLink to="" className="ms-3 topnavlink">
-                NEW
-              </NavLink>
-            </Nav>
-             <Dropdown>
-      <Dropdown.Toggle variant="dark" id="dropdown-basic " className="ms-2">
-        ALL TWO-WHEELERS
-      </Dropdown.Toggle>
+              <Navbar.Collapse id="navbarScroll">
+                <Nav className=" ms-auto my-3 my-lg-0 fw-bold " navbarScroll>
+                  <NavLink to="/" className="ms-3 topnavlink">
+                    HOME
+                  </NavLink>
+                  <NavLink to="/Newbikes" className="ms-3 topnavlink">
+                    NEW BIKES
+                  </NavLink>
+                  <NavLink to="" className="ms-3 topnavlink">
+                    NEWS&REVIEW
+                  </NavLink>
+                  <NavLink to="" className="ms-3 topnavlink">
+                    NEW
+                  </NavLink>
+                </Nav>
+                <Dropdown>
+                  <Dropdown.Toggle
+                    variant="dark"
+                    id="dropdown-basic "
+                    className="ms-2"
+                  >
+                    ALL TWO-WHEELERS
+                  </Dropdown.Toggle>
 
-      <Dropdown.Menu style={{ marginLeft:"10px", borderColor:"black"}}>
-        <Dropdown.Item href="#/action-1" className="text-dark">SUZUKI</Dropdown.Item>
-        <Dropdown.Item href="#/action-2" className="text-dark">HONDA</Dropdown.Item>
-        <Dropdown.Item href="#/action-3" className="text-dark">YAMAHA</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-          </Navbar.Collapse>
-           </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-     ))}
+                  <Dropdown.Menu
+                    style={{ marginLeft: "10px", borderColor: "black" }}
+                  >
+                    <Dropdown.Item href="#/action-1" className="text-dark">
+                      <a
+                        href="/AccessBT"
+                        style={{ textDecorationLine: "none" }}
+                      >
+                        SUZUKI
+                      </a>
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2" className="text-dark">
+                      HONDA
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3" className="text-dark">
+                      YAMAHA
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Navbar.Collapse>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </>
   );
 }
